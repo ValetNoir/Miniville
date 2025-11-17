d4fa3fc4e928189c7ec7b6938cf0751d317eff38
valet noir <valetnoir.gaming@gmail.com>
2025-11-17 10:31:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Miniville
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
