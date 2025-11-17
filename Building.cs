8ed955ca6aab5c2ed533cf4848dfde6b3d50f706
Axel <axel.riou405@gmail.com>
2025-11-17 14:45:14 +0100

    add enum script

---
+++ Building.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniville
+{
+    public enum Building
+    {
+        Ferme,
+        Champs_de_ble,
+        Boulangerie,
+        Cafe,
+        Superette,
+        Foret,
+        Fromagerie,
+        Frabrique_de_meuble,
+        Mine,
+        Restaurant,
+        Verger,
+        Marche_de_fruits_et_legumes
+    }
+}
