419ec74248c0b39965c1ecf208e9df44d54241fa
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-11-17 13:42:50 +0100

    first pile version using array

--- Program.cs
+++ Program.cs
@@ -2,11 +2,40 @@
 
 namespace Miniville
 {
+    
+    class Card {}
+    
+    class Pile
+    {
+        private Card[] Cards = [];
+        public Pile() {}
+
+        public void Stack(Card newCardReference)
+        {
+            Cards.Append(newCardReference);
+        }
+        
+        public Card Top()
+        {
+            return Cards[Cards.Length];
+        }
+        
+        public Card[] Loop()
+        {
+            return Cards;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Pile cardPile = new();
+            foreach (Card loopCard in cardPile.Loop())
+			{
+				
+			}
         }
     }
 }
