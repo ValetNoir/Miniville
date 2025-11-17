364d0e5826551e58838d1f88bc6f1b39bb158fba
Noemie Desplobain <g.tarakona@gmail.com>
2025-11-17 14:36:01 +0100

    Create Dice.cs

---
+++ Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniville
+{
+    public class Dice
+    {
+        private Random rdm;
+
+        public Dice()
+        {
+            rdm = new Random();
+        }
+
+        public int Roll()
+        {
+            int result = rdm.Next(7);
+            return result;
+        }
+    }
+}
