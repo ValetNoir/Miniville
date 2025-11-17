41bec17b7e5b50134179efb8ddda1b2dcb1662bc
Axel <axel.riou405@gmail.com>
2025-11-17 17:00:34 +0100

    game wip

--- Game.cs
+++ Game.cs
@@ -17,8 +17,12 @@ namespace Miniville
 				//Check if Player B have Blue or Red Card
 				//Check if Player A have Blue or Green Card
 				//Player A can buy a card
-			}
-		}
+				if (!Players[0].HasWon() || !Players[1].HasWon()) break;
+                //Check if Player A have Blue or Red Card
+                //Check if Player B have Blue or Green Card
+                //Player B can buy a card
+            }
+        }
 
 		public int RollDices(int dicesNumber)
 		{
