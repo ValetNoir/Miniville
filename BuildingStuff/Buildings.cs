342f01e7d68f4e6b804f6740663e0042e85e0333
valet noir <valetnoir.gaming@gmail.com>
2025-11-17 16:38:56 +0100

    added some cards

--- BuildingStuff/Buildings.cs
+++ BuildingStuff/Buildings.cs
@@ -14,10 +14,50 @@ namespace Miniville.BuildingStuff
         public readonly Card WHEAT_FIELD = new Card(
             new int[] { 1 },
             CardColor.BLUE,
-            CardType.FIELD,
+            CardType.WHEAT,
             "Wheat Field",
             "Get 1 coin from the bank. Everyone can use this card on anyone's turn.",
             1,
+            EffectsGenerator.Instance.GenerateMoney(1)
+        );
+
+        public readonly Card BAKERY = new Card(
+            new int[] { 2, 3 },
+            CardColor.GREEN,
+            CardType.SHOP,
+            "Bakery",
+            "Get 1 coin from the bank. Only on your turn.",
+            1,
+            EffectsGenerator.Instance.GenerateMoney(1)
+        );
+
+        public readonly Card CAFE = new Card(
+            new int[] { 3 },
+            CardColor.RED,
+            CardType.CUP,
+            "Cafe",
+            "Get 1 coin from the player who rolled the dice. Only on their turn.",
+            2,
+            EffectsGenerator.Instance.StealMoney(1)
+        );
+
+        public readonly Card CONVENIENCE_STORE = new Card(
+            new int[] { 4 },
+            CardColor.GREEN,
+            CardType.SHOP,
+            "Convenience Store",
+            "Get 3 coins from the bank. Only on your turn.",
+            2,
+            EffectsGenerator.Instance.GenerateMoney(3)
+        );
+
+        public readonly Card FOREST = new Card(
+            new int[] { 5 },
+            CardColor.BLUE,
+            CardType.GEAR,
+            "Forest",
+            "Get 1 coin from the bank. Everyone can use this card on anyone's turn.",
+            3,
             EffectsGenerator.Instance.GenerateMoney(1)
         );
     }
