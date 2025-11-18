3cbc7c59c1c1b4aca5285c4fc96e5222dc496d15
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-11-18 10:25:07 +0100

    working basic game loop but its not the real game juste a test it will have new commits soon. yea.

--- Game.cs
+++ Game.cs
@@ -5,7 +5,7 @@ namespace Miniville
 {
 	class Game
 	{
-		public Player[] Players = [];
+		public List<Player> Players = [];
 		public readonly int CurrentTurnPlayerIndex;
 		public Dictionary<Card, int> BuildingsAmountLeft = new Dictionary<Card, int>();
 		private Dice dice = new();
@@ -17,12 +17,12 @@ namespace Miniville
 			for (int i = 0; i < humanAmount; i++)
 			{
 				string actualName = (humanAmount > 1) ? $"humain {i+1}" : "humain";
-				Players.Append(new(PlayerType.HUMAN, actualName));
+				Players.Add(new Player(PlayerType.HUMAN, actualName));
 			}
 			for (int i = 0; i < botAmount; i++)
 			{
 				string actualName = (humanAmount > 1) ? $"bot {i+1}" : "bot";
-				Players.Append(new(PlayerType.BOT, actualName));
+				Players.Add(new Player(PlayerType.BOT, actualName));
 			}
 
 			// play
@@ -35,7 +35,7 @@ namespace Miniville
 			{
 				// get the playing player and the others
 				turns++;
-				int playingIndex = turns % Players.Length;
+				int playingIndex = turns % Players.Count;
 				Player playingPlayer = Players[playingIndex];
 				IEnumerable<Player> otherPlayers = Players.Where((val, idx) => idx != playingIndex);
 
@@ -47,6 +47,7 @@ namespace Miniville
 				//Check if PlayingPlayer have Blue or Red Card
 				
 				Console.WriteLine($"{playingPlayer} gagne {diceResult}$ ! (test)");
+				playingPlayer.Money += diceResult;
 				
 				//Check if OthersPlayers have Blue or Green Card
 				
@@ -57,7 +58,7 @@ namespace Miniville
 		public int? GetWinerIndex()
 		{
 			// get winer's index
-			for (int i = 0; i < Players.Length; i++)
+			for (int i = 0; i < Players.Count; i++)
 			{
 				if (Players[i].HasWon())
 				{
